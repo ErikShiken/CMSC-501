8c7cfc66e3966e9051844c34dcdc5be4265dfb14
Erik <Erik@Erik-PC>
2011-02-17 14:22:33 -0500

    1) Added Position class 2) Knight class - Implemented constructor, created stub for    generateThreats()

--- src/knights/Knight.java
+++ src/knights/Knight.java
@@ -1,5 +1,17 @@
 package knights;
 
-public class Knight {
+import java.util.ArrayList;
 
+public class Knight {
+	Position myPosition;
+	ArrayList<Position> threatens;
+
+	public Knight(Position pos) {
+		this.myPosition = new Position(pos.getX(), pos.getY());
+		this.generateThreats();
+	}
+
+	private void generateThreats() {
+
+	}
 }
