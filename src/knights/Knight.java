35605d485df184eadd79ffa3ac5f81621096f8b1
Erik <Erik@Erik-PC>
2011-02-17 15:17:15 -0500

    Implemented generateThreats() and getThreats()

--- src/knights/Knight.java
+++ src/knights/Knight.java
@@ -12,6 +12,32 @@ public class Knight {
 	}
 
 	private void generateThreats() {
+		// Maximum number of possible threats for a Knight is 8
+		this.threatens = new ArrayList<Position>();
+		// 'top left' positions
+		this.threatens.add(new Position(myPosition.getX() - 2, myPosition
+				.getY() + 1));
+		this.threatens.add(new Position(myPosition.getX() - 1, myPosition
+				.getY() + 2));
+		// 'top right' positions
+		this.threatens.add(new Position(myPosition.getX() + 2, myPosition
+				.getY() + 1));
+		this.threatens.add(new Position(myPosition.getX() + 1, myPosition
+				.getY() + 2));
+		// 'bottom left' positions
+		this.threatens.add(new Position(myPosition.getX() - 2, myPosition
+				.getY() - 1));
+		this.threatens.add(new Position(myPosition.getX() - 1, myPosition
+				.getY() - 2));
+		// 'bottom right' positions
+		this.threatens.add(new Position(myPosition.getX() + 2, myPosition
+				.getY() - 1));
+		this.threatens.add(new Position(myPosition.getX() + 1, myPosition
+				.getY() - 2));
+	}
 
+	@SuppressWarnings("unchecked")
+	public ArrayList<Position> getThreats() {
+		return (ArrayList<Position>) this.threatens.clone();
 	}
 }
